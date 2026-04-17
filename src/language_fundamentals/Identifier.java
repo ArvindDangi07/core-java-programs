d4aafc6d3f61940690d9ddf2150890f8b9004520
ArvindDangi07 <arvindkumar131220@gmail.com>
2026-04-17 14:41:11 +0530

    [Language Fundamentals] > Identifier.java program + concept.

---
+++ src/language_fundamentals/Identifier.java
@@ -0,0 +1,24 @@
+package language_fundamentals;
+
+public class Identifier {
+    public static void main(String[] args) {
+
+        int String = 20;
+        System.out.println(String);
+
+    }
+}
+
+/* A name in java program is identifier.
+
+# conditions we have to follow
+*  ___________________________________
+* letter should be A-Z and no. should be 0-9,_ and $.
+* it will a case sensitive.
+* Identifier cannot start with a number
+* we cant use reserved keyword as identifier.
+* but we can use predefined class name and interface as identifier.
+* but it is not recommended to use.
+* name should be lengthy.
+
+ */
